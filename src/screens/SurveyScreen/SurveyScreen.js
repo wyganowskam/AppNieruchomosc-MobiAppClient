7ae96809936ba85e3a6d88f8e798ae9049efb37b
Magdalena Wyganowska <01124618@pw.edu.pl>
2021-01-11 23:59:46 +0100

    custom tabs

--- src/screens/SurveyScreen/SurveyScreen.js
+++ src/screens/SurveyScreen/SurveyScreen.js
@@ -1,41 +1,63 @@
 import React from 'react';
-import {  ScrollView, } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, FlatList, ScrollView,StyleSheet ,Image } from 'react-native';
+import { TextInput } from "react-native-gesture-handler";
+import { Button } from 'react-native-paper';
+import { Text,Divider,Card, } from 'react-native-paper';
+import CreateSurveyAnswers from './CreateSurveyAnswers';
+import surveyService from '../../services/surveyService';
+import { FAB } from 'react-native-paper';
 import colors from "../../config/colors";
 import SurveyFill from './SurveyFillScreen';
 import SurveyResults from './SurveyResultsScreen';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-
-
-const Tab = createMaterialTopTabNavigator();
-
 
 
 export default function Survey(props) {
  
   const [value, setValue] = React.useState(0);
+  const [tab, setTab] = React.useState(0);
   const surveyId = props.route.params.surveyId;
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  function FillScreen() {
-    return (
-      <SurveyFill surveyId={surveyId}/>
-    );
-  }
-  
-  function ResultsScreen() {
-    return (
-      <SurveyResults surveyId={surveyId}/>
-    );
-  }
-
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="A" component={FillScreen} />
-      <Tab.Screen name="B" component={ResultsScreen}  />
-    </Tab.Navigator>
+    <View>
+       <View style={{flexDirection:"row",flex:1,backgroundColor:colors.white}}>
+        <Button 
+        mode="text"  
+        uppercase={true} 
+        disabled={tab===0}
+        onPress={()=> setTab(0)}
+        style={tab===0 ? styles.chosen : styles.otherone}
+        >
+          formularz
+        </Button>
+        <Button 
+        mode="text" 
+        uppercase={true} 
+        disabled={tab===1}
+        onPress={()=> setTab(1)}
+        style={tab===1 ? styles.chosen : styles.otherone}
+        >
+          wyniki
+        </Button>
+      </View>
+    <ScrollView>
+     {tab===0 ?
+      <SurveyFill surveyId={surveyId}/>
+      :
+      <SurveyResults surveyId={surveyId}/>
+     }
+      </ScrollView>
+      </View>
   );
 }
-
+const styles = StyleSheet.create({
+  chosen: {flex:1,
+      borderBottomColor:colors.violet,
+       borderBottomWidth:3},
+  otherone: {
+      flex:1,
+    },
+   
+});
